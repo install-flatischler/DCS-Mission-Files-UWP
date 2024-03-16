e24e25fa0f49916e5ace4ac7b37c842307b298c7
install-flatischler <142015836+install-flatischler@users.noreply.github.com>
2024-03-16 05:48:51 +0100

    First commit

---
+++ CopyJob.ps1
@@ -0,0 +1,22 @@
+﻿$SavedGames = "$env:Userprofile\Saved Games"
+Function Get-Folder($initialDirectory="")
+{
+    [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms")|Out-Null
+    $foldername = New-Object System.Windows.Forms.FolderBrowserDialog
+    $foldername.Description = "Select a folder"
+    $foldername.rootfolder = "MyComputer"
+    $foldername.SelectedPath = $initialDirectory
+    if($foldername.ShowDialog() -eq "OK")
+    {
+        $folder += $foldername.SelectedPath
+    }
+    return $folder
+}
+$dcsfolder = Get-Folder
+if ( -NOT (Test-Path "$dcsfolder\Mods\Aircraft")){
+    Copy-Item ".\Civil Aircraft Mod" "$dcsfolder\Mods\Aircraft\" -Recurse -Force -EA SilentlyContinue
+    Copy-Item ".\Civil Aircraft Mod" "$dcsfolder\Mods\Aircraft\" -Recurse -Force -EA SilentlyContinue
+    } ELSE {
+    Copy-Item ".\Civil Aircraft Mod\*" "$dcsfolder\Mods\Aircraft\Civil Aircraft Mod\" -Recurse -Force -EA SilentlyContinue
+    Copy-Item ".\Civil Aircraft Mod\*" "$dcsfolder\Mods\Aircraft\Civil Aircraft Mod\" -Recurse -Force -EA SilentlyContinue
+    }
